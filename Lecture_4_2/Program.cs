be1fa9f2d302cbd12cc5acf63de47a06690e2d71
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-10-19 16:42:28 +0300

    Week 4 Lectures
    Password Generator

---
+++ Lecture_4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lecture_4_2.Utilities;
+
+var passwordGenerator = new PasswordGenerator();
+
+Console.WriteLine("Lütfen şifreniz için istediğiniz karakter sayısını giriniz.");
+
+var passwordLength = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Şifreniz sayıları içersin mi ? ");
+var includeNumbers = Console.ReadLine()=="evet";
+
+Console.WriteLine("Şifreniz küçük harfler içersin mi ? ");
+var includeLowerCase = Console.ReadLine() == "evet";
+
+Console.WriteLine("Şifreniz büyük harfler içersin mi ? ");
+var includeUpperCase = Console.ReadLine() == "evet";
+
+Console.WriteLine("Şifreniz özel karakterler içersin mi ? ");
+var includeSpecialChars = Console.ReadLine() == "evet";
+
+var password = passwordGenerator.Generate(passwordLength,includeNumbers,includeLowerCase,includeUpperCase,includeSpecialChars);
+
+Console.WriteLine($"Şifreniz:{password}");
+Console.ReadLine();
+return 1;
