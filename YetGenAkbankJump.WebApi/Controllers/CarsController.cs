e7d2d2af74acad33135caf6fd8e7e2a385908786
Elif Baykara <100221534+elifbaykara@users.noreply.github.com>
2023-10-24 10:15:44 +0300

    Lecture_6_2
    [Feature]: WebAPI + Blazor + MVC Integration were added.

---
+++ YetGenAkbankJump.WebApi/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+using YetGenAkbankJump.WebApi.Data;
+
+namespace YetGenAkbankJump.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var cars = CarsContext.LuxuryCars.ToList();
+            return Ok (cars);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            if(id == Guid.Empty)
+                return BadRequest("Id cannot be empty"); //400
+
+            var car = CarsContext.LuxuryCars.FirstOrDefault(x => x.Id == id);
+
+            if (car is null)
+                return NotFound("The car requested with given Id was not found"); //404
+
+            return Ok(car);
+        }
+    }
+}
